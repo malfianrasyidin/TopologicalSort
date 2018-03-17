7e18622d2486a0ffb7db7c819a62779aa6d6fe59
deborrrrrah <dajtpbl@gmail.com>
2018-03-17 08:54:20 +0700

    Struct dan Baca File yang belum diimplementasi

---
+++ readfile.cs
@@ -0,0 +1,26 @@
+/* Tugas Besar Stima II */
+/* Rifqi Rifaldi Utomo 13516098 */
+/* Muhammad Alfian Rasyidin 13516104 */
+/* Deborah Aprilia Josephine 13516152 */
+
+namespace DirectedAcyclicGraph
+{
+    using System ;
+    public class ReadFile{
+        private string FileName;
+        private char * ReadString;
+        private char CurrChar;
+        public ReadFile(string S){
+            FileName = S;
+        }
+        public Graph openFile(string S){
+            Graph G = new Graph();
+;        }
+        public bool isComma(){
+
+        }
+        public bool isDot(){
+
+        }
+    }
+}
