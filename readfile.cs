7ee4df2495b80f07ebfe9d591d49688bd06f4648
deborrrrrah <dajtpbl@gmail.com>
2018-03-19 10:15:04 +0700

    Main

--- readfile.cs
+++ readfile.cs
@@ -3,24 +3,37 @@
 /* Muhammad Alfian Rasyidin 13516104 */
 /* Deborah Aprilia Josephine 13516152 */
 
+using System ;
+using System.IO.file ;
 namespace DirectedAcyclicGraph
 {
-    using System ;
     public class ReadFile{
-        private string FileName;
+        private string FilePath;
         private char * ReadString;
-        private char CurrChar;
+        private int CurrChar;
         public ReadFile(string S){
             FileName = S;
         }
-        public Graph openFile(string S){
+        public Graph OpenFile(string S){
             Graph G = new Graph();
-;        }
-        public bool isComma(){
-
-        }
-        public bool isDot(){
-
+            StreamReader sr = new StreamReader(path);
+            while(!sr.EndOfStream){
+                string line = sr.ReadLine();
+                string[] linePerLine = line.Split('.');
+                for (int i=0 ; i < linePerLine.Length ; i++){
+                    string[] stringPerLine = linePerLine[i].Split(',');
+                    for (int j =0; j < stringPerLine.Length ; j++){
+                        if (j==0){
+                            G.addNodes(stringPerLine[j].Trim());
+                        }
+                        else {
+                            G.addPrereqNodes(i,stringPerLine[j].Trim());
+                        }
+                    }
+                }
+            }  
+            sr.Close(); 
+            return G; 
         }
     }
 }
