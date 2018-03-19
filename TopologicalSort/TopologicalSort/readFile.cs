0d2bce0734935fce156f88952ab54185003f8667
deborrrrrah <dajtpbl@gmail.com>
2018-03-19 21:38:32 +0700

    ReadFile Done

---
+++ TopologicalSort/TopologicalSort/readFile.cs
@@ -0,0 +1,13 @@
+﻿/* Tugas Besar Strategi Algoritma II */
+/* Topological Sort */
+/* Rifqi Rifaldi Utomo (13516
+ * M Alfian 
+ * Deborah Aprilia Josephine (13516152 */
+using System;
+using System.IO;
+using System.Linq;
+
+namespace TopologicalSort
+{
+
+}
--- TopologicalSort/TopologicalSort/struct.cs
+++ TopologicalSort/TopologicalSort/struct.cs
@@ -1,20 +1,14 @@
-﻿using System;
-using System.IO;
-using System.Linq;
+﻿/* Tugas Besar Strategi Algoritma II */
+/* Topological Sort */
+/* Rifqi Rifaldi Utomo (13516
+ * M Alfian 
+ * Deborah Aprilia Josephine (13516152 */
+using System;
+using System;
 using System.Collections.Generic;
 
-class ViewerSample
+namespace TopologicalSort
 {
-    public static void Main()
-    {
-        /* Baca File */
-        ReadFile R = new ReadFile("test.txt");
-        /* Print Graph */
-        Graph G = new Graph();
-        G = R.OpenFile();
-        G.printGraph();
-    }
-
     public class Node
     {
         private string Value;
@@ -70,7 +64,8 @@ class ViewerSample
             foreach (var node in Prereq)
             {
                 Console.Write(node);
-                if (Convert.ToBoolean(string.Compare(node,Prereq[Prereq.Count - 1]))){
+                if (Convert.ToBoolean(string.Compare(node, Prereq[Prereq.Count - 1])))
+                {
                     Console.Write(", ");
                 }
             }
@@ -114,43 +109,7 @@ class ViewerSample
                 Console.WriteLine("Prerequisite : ");
                 Nodes[i].printAllPrereq();
             }
-            Console.WriteLine();
-        }
-    }
-
-    public class ReadFile
-    {
-        private string FilePath;
-        public ReadFile(string S)
-        {
-            FilePath = S;
-        }
-        public Graph OpenFile()
-        {
-            Graph G = new Graph();
-            StreamReader sr = new StreamReader(FilePath);
-            string stringText = File.ReadAllText(FilePath);
-            string[] stringPerLineDashN = stringText.Split('\n');
-            for (int i = 0; i < stringPerLineDashN.Length; i++)
-            {
-                string[] stringPerLineDot = stringPerLineDashN[i].Split('.');
-                string[] stringPerLineComma = stringPerLineDot[0].Split(',');
-                for (int k = 0; k < stringPerLineComma.Length; k++)
-                {
-                    //string s = stringPerLineComma[k].Trim();
-                    //Console.WriteLine("Array ke-{0}, indeks node ke-{1}, valuenya {2}", k, i, s);
-                    if (k == 0)
-                    {
-                        G.addNodes(stringPerLineComma[k].Trim());
-                    }
-                    else
-                    {
-                        G.addPrereqNodes(i, stringPerLineComma[k].Trim());
-                    }
-                }
-            }
-            sr.Close();
-            return G;
+            Console.WriteLine("");
         }
     }
 }
