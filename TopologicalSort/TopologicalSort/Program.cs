e12371bd7d759341c26734d656e12078b9a3ba8a
Muhammad Alfian Rasyidin <13516104@std.stei.itb.ac.id>
2018-03-20 02:36:01 +0700

    bfs dan dfs selesai dikoreksi, nmn graph masih reference

--- TopologicalSort/TopologicalSort/Program.cs
+++ TopologicalSort/TopologicalSort/Program.cs
@@ -16,7 +16,10 @@ namespace TopologicalSort
             G = R.OpenFile();
             R.GeneratePostReq(G);
             G.PrintGraph();
-            BFS bfs = new BFS(G);
+           // BFS bfs = new BFS(G);
+            Console.WriteLine("--------");
+            DFS dfs = new DFS(G);
+            dfs.Execute();
         }
     }
     public class ReadFile
@@ -69,6 +72,7 @@ namespace TopologicalSort
             }
         }
     }
+
     public class BFS
     {
         public BFS(Graph _G)
@@ -123,24 +127,7 @@ namespace TopologicalSort
             } while (belumSelesai);
         }
     }
-    public class DFS
-    {
-        private static int timestamp;
-        private List<string> reverseorder = new List<string>();
-        public static int GetTimestamp()
-        {
-            return timestamp;
-        }
-        public static void SetTimestamp(int s)
-        {
-            timestamp = s;
-        }
-        public DFS(Graph _G)
-        {
-            Graph G = _G;
 
-        }
-    }
     public class Node
     {
         private string Value;
@@ -229,7 +216,7 @@ namespace TopologicalSort
                 }
             }
         }
-        public void printAllPostreq()
+        public void PrintAllPostreq()
         {
             foreach (var node in Postreq)
             {
@@ -239,6 +226,11 @@ namespace TopologicalSort
                     Console.Write(", ");
                 }
             }
+        }
+
+        public int GetNumberPostReq()
+        {
+            return Postreq.Count;
         }
     }
 
@@ -304,13 +296,110 @@ namespace TopologicalSort
                 Console.WriteLine("Prerequisite : ");
                 Nodes[i].PrintAllPrereq();
                 Console.WriteLine("\nPostrequisite : ");
-                Nodes[i].printAllPostreq();
+                Nodes[i].PrintAllPostreq();
             }
             Console.WriteLine("");
         }
         public void NodesRemoveAt(int Idx)
         {
             Nodes.RemoveAt(Idx);
+        }
+
+        public int Count()
+        {
+            return Nodes.Count;
+        }
+    }
+
+    public class DFS
+    {
+        private int number_of_nodes;
+        List<string> timestamps, first_semester;
+        private Graph graph;
+
+        public DFS (Graph _graph)
+        {
+            graph = _graph;
+            number_of_nodes = graph.Count();
+            timestamps = new List<string>();
+            first_semester = new List<string>();
+        }
+
+        public void InitFirstSemester()
+        {
+            for (int i = 0; i < graph.Count(); i++)
+            {
+                if (graph.GetNodes(i).GetPrereqCount() == 0)
+                {
+                    first_semester.Add(graph.GetNodes(i).GetVal());
+                }
+            }
+        }
+
+        public void Execute()
+        {
+            InitFirstSemester();
+            for (int i = 0; i < first_semester.Count; i++)
+            {
+                string current = first_semester[i];
+                //Console.WriteLine(current);
+                ExecuteDFS(graph, current);
+            }
+            Print();
+        }
+
+        public void ExecuteDFS(Graph graph, string current)
+        {
+            timestamps.Add(current);
+            int idx = graph.GetNodesIdx(current);
+            //Console.WriteLine("idx" + idx.ToString());
+            Node x = graph.GetNodes(idx);
+            if (x.GetNumberPostReq() != 0)
+            {
+                //Console.WriteLine(x.GetNumberPostReq());
+                for (int i = 0; i < x.GetNumberPostReq(); i++)
+                {
+                    //Console.WriteLine(i);
+
+                    if (!HasVisited(x.GetPostreq(i)))
+                    {
+                        //Console.WriteLine("yes");
+                        ExecuteDFS(graph, x.GetPostreq(i));
+                    }
+                }
+                timestamps.Add(current);
+            }
+            else
+            {
+                timestamps.Add(current);
+            }
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < timestamps.Count; i++)
+            {
+                Console.WriteLine(timestamps[i]);
+            }
+        }
+
+        public bool HasVisited(string current)
+        {
+            bool found = false;
+            int i = 0;
+            //Console.WriteLine("here");
+            while (!found && i < timestamps.Count)
+            {
+                if (timestamps[i].Equals(current, StringComparison.Ordinal))
+                {
+                    found = true;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            return found;
         }
     }
 }
